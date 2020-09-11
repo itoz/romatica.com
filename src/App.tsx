8bc69795b73867b910ad0ae781194b82c78d7bca
itokazu masafumi <romatica@gmail.com>
2020-09-12 01:36:03 +0900

    add first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header className="App-header"></header>
+      <footer>
+        当サイト（www.romatica.com/以下）に掲載されている内容はitozが完全に個人的立場で掲載するもので、私の所属する団体などの見解とは一切関係ありません。<br />
+        また、すべての内容はその掲載当時における個人的見解ですので、その正確性については一切保証致しません。<br />
+        <small>
+          copytight©2004-2014 RomaticA.com All rights reserved. 
+        </small>
+      </footer>
     </div>
   );
 }
